4acafc6ff6f43d0795dc7ae6740d964790ef8ab5
ybnr1130 <muuckk@gmail.com>
2021-08-11 12:24:05 +0000

    [Updete]3Finish

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'be84378b980f81d5a90b4052463418adff8cc9ac59298ea2b1fb97cc8c24b251366dc211078025c2a72f39f551e395b12d94e8bf5b242284a09ccf3a65c8b910'
